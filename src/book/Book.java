2f5195f0c79c03a440c20c8ef94749c6bd4c7790
kirkpreston <prestokp@rose-hulman.edu>
2019-06-16 14:51:01 -0500

    Bookshelf Java Programming Assigment

---
+++ src/book/Book.java
@@ -0,0 +1,25 @@
+package book;
+
+public class Book {
+
+  public Book(){}
+
+  public void setBookData(){
+
+    String crazyRichTitle = "Crazy Rich Asians";
+    String chinaRichTitle = "China Rich Girlfriend";
+    String richPeopleTitle = "Rich People Problems";
+    String authorName = "Kevin Kwan";
+    String publisherName = "Penguin Random House LLC";
+
+  }
+  public int copyrightDate(){
+    
+  }
+
+  public void setCopyrightDate(int date1, int date2, int date3){
+
+
+
+  }
+}
---
+++ src/book/Bookshelf.java
@@ -0,0 +1,14 @@
+package book;
+
+public class Bookshelf {
+  public static void main(String[] args) {
+
+    Book crazyRich, chinaRich, richPeople; //creates the reference variables to the three books on my shelf of type book
+
+    crazyRich = new Book();
+    chinaRich = new Book();
+    richPeople = new Book();
+
+
+  }
+}
